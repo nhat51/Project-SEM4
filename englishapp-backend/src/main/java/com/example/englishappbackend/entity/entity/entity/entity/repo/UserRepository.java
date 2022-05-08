82ef0babd2b43b7b15115835b1f400fc6c71456a
nhat51 <65001571+nhat51@users.noreply.github.com>
2022-05-08 18:12:30 +0700

    fix duplicate

--- englishapp-backend/src/main/java/com/example/englishappbackend/entity/entity/entity/entity/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.englishappbackend.entity.entity.entity.entity.repo;
-
-import com.example.englishappbackend.entity.entity.entity.entity.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- englishapp-backend/src/main/java/com/example/englishappbackend/entity/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.englishappbackend.entity.repo;
-
-import com.example.englishappbackend.entity.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
