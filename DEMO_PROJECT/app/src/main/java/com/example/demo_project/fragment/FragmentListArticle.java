6e573ec81b7cd45438ab23e96de855da81cb8402
nguyenhuong301201 <nguyenhuong301201@gmail.com>
2022-06-01 19:05:27 +0700

    update article

--- DEMO_PROJECT/app/src/main/java/com/example/demo_project/fragment/FragmentListArticle.java
+++ DEMO_PROJECT/app/src/main/java/com/example/demo_project/fragment/FragmentListArticle.java
@@ -1,6 +1,5 @@
 package com.example.demo_project.fragment;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -10,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -41,11 +41,12 @@ public class FragmentListArticle extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         currentContext = container.getContext();
         view = inflater.inflate(R.layout.fragment_list_article, container, false);
-        initView();
         initData();
+        initView();
+        initBackViewArticle();
+        initSettingViewArticle();
         return view;
     }
-
     private void initView() {
         rvArticle = view.findViewById(R.id.rvArticle);
         rvArticle.setLayoutManager(new LinearLayoutManager(currentContext));
@@ -54,18 +55,44 @@ public class FragmentListArticle extends Fragment {
     private void initData() {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
         articles = new ArrayList<>();
         if (articleService == null){
             articleService = RetrofitGenerator.createService(ArticleService.class);
         }
         try {
-            Response<ArticleResponse> listResponse = articleService.getAllArticle().execute();
-            Log.d("LOG: ", String.valueOf(listResponse.body().getContent().size()));
-            if (listResponse.isSuccessful()){
-                articles.addAll(listResponse.body().getContent());
+            Response<ArticleResponse> listArticleResponse = articleService.getAllArticle().execute();
+            Log.d("LOG: ", String.valueOf(listArticleResponse.body().getContent().size()));
+            if (listArticleResponse.isSuccessful()){
+                articles.addAll(listArticleResponse.body().getContent());
             }
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+    private void initSettingViewArticle() {
+        btn_setting_list_article = view.findViewById(R.id.btn_setting_list_article);
+        btn_setting_list_article.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_setting_list_article.setOnClickListener(view1 -> getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frameLayout, MainActivity.fragmentSetting, FragmentSetting.class.getName())
+                        .commit());
+            }
+        });
+    }
+
+    private void initBackViewArticle() {
+        btn_back_list_article = view.findViewById(R.id.btn_back_list_article);
+        btn_back_list_article.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_back_list_article.setOnClickListener(view1 -> getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frameLayout, MainActivity.fragmentHome, FragmentHome.class.getName())
+                        .commit());
+            }
+        });
     }
 }
--- DEMO_PROJECT/app/src/main/java/com/example/demo_project/service/ArticleService.java
+++ DEMO_PROJECT/app/src/main/java/com/example/demo_project/service/ArticleService.java
@@ -1,12 +1,18 @@
 package com.example.demo_project.service;
 
+import com.example.demo_project.entity.Article;
 import com.example.demo_project.entity.ArticleResponse;
+import com.example.demo_project.entity.Word;
 import com.example.demo_project.entity.WordResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ArticleService {
     @GET("api/v1/articles")
     Call<ArticleResponse> getAllArticle();
+
+    @GET("api/v1/article/article-detail?")
+    Call<Article> getArticleDetail(@Query("article-id") int articleId);
 }
